0e69f3c3f2aa19e947a41bd9355e7df95a3edd83
Mrsosann <243906076@qq.com>
2017-07-04 22:45:41 +0800

    init Nav component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,6 +12,7 @@ const { Header, Content, Footer } = Layout;
 
 import Home from './Home';
 import Sale from './Sale';
+import Nav from './Nav';
 
 const iconUser = (
     <Icon type="user" style={{marginRight: '8px'}}/>
@@ -38,60 +39,81 @@ const signin = (
         </Menu.Item>
     </Menu>
 );
-
 class App extends React.Component {
-  constructor() {
-    super();
-  }
-  saleClick () {
-    console.log('sale is clicked');
-    this.props.history.push('/sale/chuzhu');
-  }
   render() {
     return (
-      <Layout>
-        <Header style={{ position: 'fixed', width: '100%', height: '46px' }}>
-            <div className="logo" />
-            <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['1']}
-            style={{ lineHeight: '46px' }}
-            >
-            <Menu.Item key="1">
-                Home
-            </Menu.Item>
-            <Menu.Item key="2" >
-                <span onClick={this.saleClick} ><Icon type="mail" />Guide</span>
-            </Menu.Item>
-            <Menu.Item key="3">Introduce</Menu.Item>
-            <Menu.Item style={{ float:'right' }}>
-                <Dropdown overlay={signin}>
-                    <a className="ant-dropdown-link" href="#">
-                    Sign in <Icon type="down" style={{marginRight: '0'}}/>
-                    </a>
-                </Dropdown>
-            </Menu.Item>
-            <Menu.Item key="4" style={{ float:'right' }}><Icon type="user-add" /> Sign up</Menu.Item>
-            </Menu>
-        </Header>
-
-        <Content style={{ padding: '0 50px', marginTop: 46 }}>
-            <Router>
-                <div>
-                  <Route exact path="/" component={Home}/>
-                  <Route path="/index" component={Home}/>
-                  <Route path="/sale/:id" component={Sale}/>
-                </div>
-            </Router>
-        </Content>
-
-        <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©2016 Created by Ant UED
-        </Footer>
-      </Layout>
+      <Router>
+        <Layout>
+          <Nav />
+          <Content style={{ padding: '0 50px', marginTop: 64 }}>
+            <Route exact path="/" component={Home}/>
+            <Route path="/index" component={Home}/>
+            <Route path="/sale/:id" component={Sale}/>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Copy ©2017 Created by MrSosann &nbsp;&nbsp;
+            <Icon type="github" />
+            <a href="https://github.com/Mrsosann/production-management" title="Click to Github address">&nbsp;Github</a>
+          </Footer>
+        </Layout>
+      </Router>
     );
   }
 }
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//   }
+//   saleClick () {
+//     console.log('sale is clicked');
+//     this.props.history.push('/sale/chuzhu');
+//   }
+//   render() {
+//     return (
+//       <Layout>
+//         <Header style={{ position: 'fixed', width: '100%', height: '46px' }}>
+//             <div className="logo" />
+//             <Menu
+//             theme="dark"
+//             mode="horizontal"
+//             defaultSelectedKeys={['1']}
+//             style={{ lineHeight: '46px' }}
+//             >
+//             <Menu.Item key="1">
+//                 Home
+//             </Menu.Item>
+//             <Menu.Item key="2" >
+//                 <span onClick={this.saleClick} ><Icon type="mail" />Guide</span>
+//             </Menu.Item>
+//             <Menu.Item key="3">Introduce</Menu.Item>
+//             <Menu.Item style={{ float:'right' }}>
+//                 <Dropdown overlay={signin}>
+//                     <a className="ant-dropdown-link" href="#">
+//                     Sign in <Icon type="down" style={{marginRight: '0'}}/>
+//                     </a>
+//                 </Dropdown>
+//             </Menu.Item>
+//             <Menu.Item key="4" style={{ float:'right' }}><Icon type="user-add" /> Sign up</Menu.Item>
+//             </Menu>
+//         </Header>
+
+//         <Content style={{ padding: '0 50px', marginTop: 46 }}>
+//             <Router>
+//                 <div>
+//                   <Route exact path="/" component={Home}/>
+//                   <Route path="/index" component={Home}/>
+//                   <Route path="/sale/:id" component={Sale}/>
+//                 </div>
+//             </Router>
+//         </Content>
+
+//         <Footer style={{ textAlign: 'center' }}>
+//             Ant Design ©2016 Created by Ant UED
+//         </Footer>
+//       </Layout>
+//     );
+//   }
+// }
 
 export default App
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,58 +1,71 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import { Menu, Icon, Breadcrumb, Layout, Dropdown } from 'antd';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
-// 固定头部
 const { Header, Content, Footer } = Layout;
-
-const iconUser = (
-    <Icon type="user" style={{marginRight: '8px'}}/>
-);
-const signin = (
-    <Menu theme="dark">
-        <Menu.Item key="0" disabled>Select role to Sign in</Menu.Item>
-        <Menu.Divider />
-        <Menu.Item key="1">
-        <a target="_blank" rel="noopener noreferrer">
-            {iconUser}SalesMan</a>
-        </Menu.Item>
-        <Menu.Item key="2">
-        <a target="_blank" rel="noopener noreferrer">
-            {iconUser}ProductAdmin</a>
-        </Menu.Item>
-        <Menu.Item key="3">
-        <a target="_blank" rel="noopener noreferrer">
-            {iconUser}WarehouseMan</a>
-        </Menu.Item>
-        <Menu.Item key="4">
-        <a target="_blank" rel="noopener noreferrer">
-            {iconUser}Worker</a>
-        </Menu.Item>
-    </Menu>
-    );
+// import LoginModal from './LoginModal';
 
 class Nav extends React.Component {
+  handleLoginModalClick = (e) => {
+    console.log(e.key);
+    if (e.key === '3') {
+      this.setState({ visible: false });
+    }
+  }
   render() {
+      let self = this;
+      const iconUser = (
+        <Icon type="user" style={{marginRight: '8px'}}/>
+      )
+      const loginModal = (
+        <Menu theme="dark" onClick={self.handleLoginModalClick}>
+            <Menu.Item key="0" disabled>Select role to Sign in</Menu.Item>
+            <Menu.Divider />
+            <Menu.Item key="1">
+                {iconUser}SalesMan
+            </Menu.Item>
+            <Menu.Item key="2">
+                {iconUser}ProductAdmin
+            </Menu.Item>
+            <Menu.Item key="3">
+                {iconUser}WarehouseMan
+            </Menu.Item>
+            <Menu.Item key="4">
+                {iconUser}Worker
+            </Menu.Item>
+        </Menu>
+    )
+    
     return (
-      <Layout>
-        <Header style={{ position: 'fixed', width: '100%', height: '46px' }}>
+        <Header style={{ position: 'fixed', width: '100%', fontSize: 14}}>
             <div className="logo" />
             <Menu
             theme="dark"
             mode="horizontal"
             defaultSelectedKeys={['1']}
-            style={{ lineHeight: '46px' }}
+            style={{ lineHeight: '64px', width: '60%', display:'inline-block'}}
             >
             <Menu.Item key="1">
-                Home
+                <Link to="/sale/chuzhu">Home</Link>
             </Menu.Item>
             <Menu.Item key="2">
                 <Icon type="mail" />Guide
             </Menu.Item>
             <Menu.Item key="3">Introduce</Menu.Item>
-            <Menu.Item style={{ float:'right' }}>
-                <Dropdown overlay={signin}>
+            </Menu>
+            <Menu
+            theme="dark"
+            mode="horizontal"
+            style={{ lineHeight: '64px', width: '40%', display:'inline-block'}}
+            >
+            <Menu.Item key="5" style={{ float:'right' }}>
+                <Dropdown overlay={loginModal}>
                     <a className="ant-dropdown-link" href="#">
                     Sign in <Icon type="down" style={{marginRight: '0'}}/>
                     </a>
@@ -61,20 +74,10 @@ class Nav extends React.Component {
             <Menu.Item key="4" style={{ float:'right' }}><Icon type="user-add" /> Sign up</Menu.Item>
             </Menu>
         </Header>
-        <Content style={{ padding: '0 50px', marginTop: 46 }}>
-            <Breadcrumb style={{ margin: '12px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©2016 Created by Ant UED
-        </Footer>
-      </Layout>
     );
   }
 }
+
+
 
 export default Nav
